e3545d366ecf5429110217af6d72a9bea6367088
Nazim Uddin <107185368+Nazim6269@users.noreply.github.com>
2024-03-21 00:12:58 +0600

    added radio input functionality

--- src/Components/task/Task.jsx
+++ src/Components/task/Task.jsx
@@ -6,6 +6,7 @@ const init = {
   text: "",
   checked: false,
   group: "home",
+  priority: "low",
 };
 
 const Task = () => {
@@ -33,6 +34,31 @@ const Task = () => {
           value={formState.text.value}
           onChange={handleChange}
         />
+        <select name="group" onChange={handleChange}>
+          <option value="home">Home</option>
+          <option value="office">Office</option>
+        </select>
+        <input
+          type="radio"
+          name="priority"
+          value={"low"}
+          onChange={handleChange}
+        />
+        Low
+        <input
+          type="radio"
+          name="priority"
+          value={"medium"}
+          onChange={handleChange}
+        />
+        Medium
+        <input
+          type="radio"
+          name="priority"
+          value={"high"}
+          onChange={handleChange}
+        />
+        High
         <Button>submit</Button>
       </form>
     </div>
